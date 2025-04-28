1110f31c01516f66c677b31d2ff222ff3e4351b6
Ivan Bobrov <ibobrovs9@gmail.com>
2025-04-28 22:45:31 +0300

    Initial commit

---
+++ publicposts/publicposts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from publicpostsapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/posts/', views.posts),
+    path('api/like_post/<int:post_id>/', views.like_post),
+]
